c5060106c9e5e5e541e92276bb7fc93e27f5dc3c
Guilherme <guiegle@hotmail.com>
2022-02-10 01:59:19 -0300

    recipes

---
+++ src/doctor/doctor.ts
@@ -0,0 +1,9 @@
+import { Prop } from "@nestjs/mongoose";
+import { Role } from "src/role/role";
+
+export class Doctor extends Role{
+    @Prop()
+    crm: string;
+    @Prop()
+    cqe: string; 
+}
